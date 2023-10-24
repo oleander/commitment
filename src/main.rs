844b8ff23c0d7bca0073e5291b1e2fa172199b3c
Linus Oleander <linus.oleander@clark.de>
2023-10-24 06:47:27 +0200

    Add .cargo/config.toml and Cargo.toml, refactor commit function

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,12 @@
+
 use anyhow::anyhow;
+use anyhow::bail;
 use anyhow::Context;
 use anyhow::Result;
-use anyhow::bail;
 use git2::{IndexAddOption, Repository, StatusOptions};
 use regex::Regex;
 
-trait Ticket {
+pub(crate) trait Ticket {
   fn to_ticket(&self) -> (Option<&str>, Option<&str>);
 }
 
@@ -28,7 +29,7 @@ impl Ticket for str {
 }
 
 // Capitalize the first letter of a string
-fn capitalize_first(s: &str) -> String {
+pub(crate) fn capitalize_first(s: &str) -> String {
   let mut c = s.chars();
   match c.next() {
     None => String::new(),
@@ -44,7 +45,7 @@ fn capitalize_first(s: &str) -> String {
 // * Message              -> Message
 // * ABC-123x             -> ABC-123
 // * ABC-123-NOPE         -> ABC-123
-fn commit(br: &str, msg: &str) -> Result<String> {
+pub(crate) fn commit(br: &str, msg: &str) -> Result<String> {
   match (br.to_ticket(), msg.to_ticket()) {
     ((Some(t1), _), (Some(t2), _)) if t1 != t2 => bail!("Branch and message tickets do not match".to_string()),
 
@@ -59,7 +60,7 @@ fn commit(br: &str, msg: &str) -> Result<String> {
 }
 
 // Check if there are any uncommitted changes
-fn has_changes(repo: &Repository) -> Result<bool, anyhow::Error> {
+pub(crate) fn has_changes(repo: &Repository) -> Result<bool, anyhow::Error> {
   let mut options = StatusOptions::new();
   options.include_untracked(true).recurse_untracked_dirs(true);
 
@@ -135,4 +136,65 @@ fn main() -> anyhow::Result<()> {
   }
 
   Ok(())
+}
+
+// #[test]
+// fn test_to_commit() {
+//   assert_eq!(
+//     commit("INVALID INVALID", "ABC-123 Message"),
+//     Ok("ABC-123 Message".to_string())
+//   );
+//   assert_eq!(
+//     commit("INVALID", "ABC-123 Message"),
+//     Ok("ABC-123 Message".to_string())
+//   );
+//   assert_eq!(
+//     commit("", "ABC-123 Message"),
+//     Ok("ABC-123 Message".to_string())
+//   );
+//   assert_eq!(
+//     commit("ABC-123", "Message"),
+//     Ok("ABC-123 Message".to_string())
+//   );
+//   assert_eq!(
+//     commit("ABC-123-DEF", "Tail"),
+//     Ok("ABC-123 Tail".to_string())
+//   );
+//   assert_eq!(commit("", "message"), Ok("Message".to_string()));
+//   assert_eq!(commit("X", "message"), Ok("Message".to_string()));
+//   assert_eq!(
+//     commit("ABC-123", "message"),
+//     Ok("ABC-123 Message".to_string())
+//   );
+//   assert!(commit("ABC-123", "ABC-123 Tail1 Tail2").is_ok());
+//   assert!(commit("ABC-123", "DEF-456 Tail").is_err());
+//   assert!(commit("ABC-123", "ABC-123 Tail").is_ok());
+//   assert!(commit("ABC-123", "DEF-456").is_err());
+//   assert!(commit("ABC-123-DEF", "").is_err());
+//   assert!(commit("ABC-123", "").is_err());
+//   assert!(commit("", "").is_err());
+// }
+
+#[test]
+fn test_capitalize_first() {
+  assert_eq!(capitalize_first("abc"), "Abc");
+  assert_eq!(capitalize_first("Abc"), "Abc");
+  assert_eq!(capitalize_first(""), "");
+}
+
+#[test]
+fn test_to_ticket() {
+  assert_eq!(
+    "Head Tail1 Tail2 Tail3".to_ticket(),
+    (None, Some("Head Tail1 Tail2 Tail3"))
+  );
+  assert_eq!(
+    "ABC-123 Tail1 Tail2".to_ticket(),
+    (Some("ABC-123"), Some("Tail1 Tail2"))
+  );
+  assert_eq!("ABC-123 Tail".to_ticket(), (Some("ABC-123"), Some("Tail")));
+  assert_eq!("ABC-123".to_ticket(), (Some("ABC-123"), None));
+  assert_eq!("ABC-123".to_ticket(), (Some("ABC-123"), None));
+  assert_eq!("Head".to_ticket(), (None, Some("Head")));
+  assert_eq!("".to_ticket(), (None, None));
 }
